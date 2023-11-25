af7373708574eb3fcb6dbd76a5c87661c22733dc
trevor-cheung <t6cheung@uwaterloo.ca>
2023-11-25 17:37:55 -0500

    initial test with just printing the transcript

---
+++ test.py
@@ -0,0 +1,40 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import taipy as tp
+
+from taipy import Config
+
+def build_message(input: str):
+    message = YouTubeTranscriptApi.get_transcript(input.split("=")[-1], languages=['ko'])
+    string = ''
+    for i in message:
+        print(i['text'])
+        string += i['text'] + " "
+    return string
+
+input_name_data_node_cfg = Config.configure_data_node(id="input_name")
+message_data_node_cfg = Config.configure_data_node(id="message")
+build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
+scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
+
+
+page = """
+Name: <|{input_name}|input|>
+<|submit|button|on_action=submit_scenario|>
+Message: <|{message}|text|>
+"""
+
+input_name = "Taipy"
+message = None
+
+def submit_scenario(state):
+    state.scenario.input_name.write(state.input_name)
+    state.scenario.submit(wait=True)
+    state.message = scenario.message.read()
+
+if __name__ == "__main__":
+    tp.Core().run()
+    scenario = tp.create_scenario(scenario_cfg)
+    tp.Gui(page).run()
+
+
+
