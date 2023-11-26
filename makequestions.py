925e602a839207f354247ad8b3b46415fdc15bb1
Adrian Tang <adrian.s.tang@gmail.com>
2023-11-25 23:38:08 -0500

    added confidence evaluation and feedback to answers

--- makequestions.py
+++ makequestions.py
@@ -4,14 +4,7 @@ import config as config
 import re
 from youtube_transcript_api import YouTubeTranscriptApi
 
-
-def link_to_transcript(link): # takes link, returns transcript
-    video_id = link.replace('https://www.youtube.com/watch?v=', '')
-    script = YouTubeTranscriptApi.get_transcript(video_id)
-    return script
-
-
-def generate_questions(transcript): # takes transcript, returns q/a as str
+def chatgpt(prompt):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer '+config.openai_key,
@@ -22,7 +15,7 @@ def generate_questions(transcript): # takes transcript, returns q/a as str
         'messages': [
             {
                 'role': 'user',
-                'content': f'Write a set of questions in English based on the script to test a reader\'s comprehension of the script. Include answers to the question. Preceed each question with \'Q:\' and each answer with \'A:\'\nTranscript: \n{transcript}',
+                'content': f'{prompt}',
             },
         ],
         'temperature': 0.5,
@@ -34,6 +27,17 @@ def generate_questions(transcript): # takes transcript, returns q/a as str
     # print(response_dict)  
 
     return(response_dict['choices'][0]['message']['content'])
+
+def link_to_transcript(link): # takes link, returns transcript
+    video_id = link.replace('https://www.youtube.com/watch?v=', '')
+    script = YouTubeTranscriptApi.get_transcript(video_id)
+    return script
+
+
+def generate_questions(transcript): # takes transcript, returns q/a as str
+    prompt = f"Write a set of questions in English based on the script to test a reader\'s comprehension of the script. Include answers to the question. Preceed each question with \'Q:\' and each answer with \'A:\'\nTranscript: \n{transcript}"
+
+    return chatgpt(prompt)
 
 
 def Q_and_A_arrays(str): # takes str, return q/a arrays
@@ -58,11 +62,17 @@ def Q_and_A_arrays(str): # takes str, return q/a arrays
 def linkToQs(link): # takes link, returns q/a arrays
     return Q_and_A_arrays(generate_questions(link_to_transcript(link)))
 
+def confidence(string, ans): # gives two values: evaluation number as int and feedback as string.
+    prompt = f"Compare how accurate the following response is to the actual answer. Evaluate leniently with regard to specific specific wording - it is more important that the individual understands the general idea. \nIf the response is completely accurate, say \'2\'. If the response is fairly accurate with few errors, say \'1\'. If the reponse is mostly or completely incorrect, say \'0\'. \n Begin by saying the evaluation number (e.g 0,1,2) followed by an explanation for why you gave that evaluation. Speak as if you are speaking directly to the respondent. \nResponse: {string}\nActual answer: {ans}\n"
+    output = chatgpt(prompt)
+    evaluation = output[0]
+    feedback = output.replace(str(evaluation), "", 1)
+    return evaluation, feedback.strip()
+        
+
 # link = "https://www.youtube.com/watch?v=sbIQLzieUq8"
 
 # qs, ans = linkToQs(link)
 
-
-
-
-
+# conf, feedback = confidence("theshy said he should have been more reckless in the beginning and that he could have possibly won in that first gank if he had been more aggressive", "TheShy regrets not being more reckless during the first phase when the enemy team went for a top gank. He feels he could have been more aggressive and potentially won that attempt.")
+# print(conf+"\n"+feedback)
